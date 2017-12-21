9c50c9885714518a4ec910cfbaa281ec1e9e45ed
Mr-Hobbes <niguti1@gmail.com>
2017-12-21 09:49:25 +0100

    x

--- fornetti/app.js
+++ fornetti/app.js
@@ -4,11 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var http = require('http');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var server = http.createServer(app);
+server.listen(3000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- temp/config.js
+++ temp/config.js
@@ -2,4 +2,4 @@ const databaseSettings = {
     password: "Z18CFxFAw4szBxJF",
     user: "fornetti",
     database: "fornetti"
-}
+};
